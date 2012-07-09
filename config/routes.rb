ef2a040b838166a47b354703e0665baa137303e6
charusingh <charu.singh@synechron.com>
2012-07-09 13:25:23 +0530

    adding the friend list on profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Social::Application.routes.draw do
+
+
+
+
+  get "home/index"
+
+  match "/users/edit_profile/:id" => "users#edit_profile", :as => "edit_profile"
+
+  devise_for :users
+
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -42,17 +53,17 @@ Social::Application.routes.draw do
   # Sample resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
+   # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+   match ':controller(/:action(/:id(.:format)))'
 end
